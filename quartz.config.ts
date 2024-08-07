9f6673062116beca8ffba49915acf9f8453c2f35
Gabriel Chartier <gabriel@chartier.link>
2024-08-07 11:04:29 -0700

    Quartz sync: Aug 7, 2024, 11:04 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "⚛️ Seption",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
